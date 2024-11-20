287f898e6ac0555df2f004e927fd9776b90fd5c3
adarshgupta0601@gmail.com <escape>
2024-11-20 18:31:54 +0530

    completed frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
-import Doctor from './pages/doctor'
+import Doctor from './pages/Doctor'
 import Login from './pages/Login'
 import About from './pages/About'
 import Contact from './pages/contact'
